e24015bdbf7553146f7d8c363a591ae777b2b993
Андрей Иващенко <xxx3003@gmail.com>
2020-02-14 16:17:08 +0300

    commit

--- Newton_method.go
+++ Newton_method.go
@@ -5,9 +5,20 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	return 0.0
+	x1 := x / 2
+	x2 := 0.0
+
+	for {
+		x2 = 0.5 * (x1 + x/x1)
+		fmt.Println(x2)
+
+		if (x1 - x2) < 0.00000000000001 {
+			return x1
+		}
+		x1 = x2
+	}
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(10))
 }
