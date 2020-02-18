842eb91e14f43301f2804ce3aaa93ffb2ea09057
Андрей Иващенко <xxx3003@gmail.com>
2020-02-18 12:31:32 +0300

    commit

--- coursera/hw1_tree/main.go
+++ coursera/hw1_tree/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	// TODO
-	// ...
+	fileObj, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Could not open %s: %s", path, err.Error())
+	}
+	defer fileObj.Close()
+	fileName := fileObj.Name()
+	files, err := ioutil.ReadDir(fileName)
+	if err != nil {
+		log.Fatalf("Could not read dir names in %s: %s", path, err.Error())
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Fprintf(out, "%s\n", file.Name())
+			newDir := filepath.Join(path, file.Name())
+			dirTree(out, newDir, printFiles)
+		} else if printFiles {
+			if file.Size() > 0 {
+				fmt.Fprintf(out, "%s (%vb)\n", file.Name(), file.Size())
+			} else {
+				fmt.Fprintf(out, "%s (empty)\n", file.Name())
+			}
+		}
+	}
 	return nil
 }
 
@@ -16,7 +41,7 @@ func main() {
 	if !(len(os.Args) == 2 || len(os.Args) == 3) {
 		panic("usage go run main.go . [-f]")
 	}
-	path := os.Args[1]
+	path := "testdata" //os.Args[1]
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
 	err := dirTree(out, path, printFiles)
 	if err != nil {
