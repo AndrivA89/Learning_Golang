c92bc6add01b61de7fe4e4d435e53e1663a0472d
Андрей Иващенко <xxx3003@gmail.com>
2020-02-20 13:42:49 +0300

    commit

--- coursera/hw2_signer/signer.go
+++ coursera/hw2_signer/signer.go
@@ -28,28 +28,41 @@ func worker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
 	job(in, out)
 }
 
+// Воркер для SingleHash. По аналогии с job
+func workerSingleHash(in int, out chan interface{}, mu *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	data := strconv.Itoa(in)
+	mu.Lock() // чтобы не было "OverheatLock happend"
+	dataMd5 := DataSignerMd5(data)
+	mu.Unlock()
+	dataCrc32Md5 := DataSignerCrc32(dataMd5)
+	dataCrc32 := DataSignerCrc32(data)
+	fmt.Printf("%s SingleHash data %s\n", data, data)
+	fmt.Printf("%s SingleHash md5(data) %s\n", data, dataMd5)
+	fmt.Printf("%s SingleHash crc32(md5(data)) %s\n", data, dataCrc32Md5)
+	fmt.Printf("%s SingleHash crc32(data) %s\n", data, dataCrc32)
+	fmt.Printf("%s SingleHash result %s\n", data, dataCrc32+"~"+dataCrc32Md5)
+	out <- dataCrc32 + "~" + dataCrc32Md5
+}
+
 var SingleHash = func(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
 	for i := range in {
-		data := strconv.Itoa(i.(int))
-		md5Data := DataSignerMd5(data)
-		crc32Md5Data := DataSignerCrc32(md5Data)
-		crc32Data := DataSignerCrc32(data)
-		fmt.Printf("%s SingleHash data %s\n", data, data)
-		fmt.Printf("%s SingleHash md5(data) %s\n", data, md5Data)
-		fmt.Printf("%s SingleHash crc32(md5(data)) %s\n", data, crc32Md5Data)
-		fmt.Printf("%s SingleHash crc32(data) %s\n", data, crc32Data)
-		fmt.Printf("%s SingleHash result %s\n", data, crc32Data+"~"+crc32Md5Data)
-		out <- crc32Data + "~" + crc32Md5Data
+		wg.Add(1)
+		go workerSingleHash(i.(int), out, mu, wg)
 	}
+	wg.Wait()
 }
 
 var MultiHash = func(in, out chan interface{}) {
 	for i := range in {
 		var sum = ""
 		for j := 0; j < 6; j++ {
-			crc32Data := DataSignerCrc32(strconv.Itoa(j) + i.(string))
-			fmt.Printf("%s MultiHash: crc32(th+step1)) %d %s\n", i.(string), j, crc32Data)
-			sum += crc32Data
+			dataCrc32 := DataSignerCrc32(strconv.Itoa(j) + i.(string))
+			fmt.Printf("%s MultiHash: crc32(th+step1)) %d %s\n", i.(string), j, dataCrc32)
+			sum += dataCrc32
 		}
 		fmt.Printf("%s MultiHash result:\n%s\n", i.(string), sum)
 		out <- sum
@@ -57,14 +70,9 @@ var MultiHash = func(in, out chan interface{}) {
 }
 
 var CombineResults = func(in, out chan interface{}) {
-	var count = 0
 	var array []string
 	for i := range in {
-		count++
 		array = append(array, i.(string))
-		if count > 6 {
-			close(in)
-		}
 	}
 	sort.Strings(array)
 	result := strings.Join(array, "_")
